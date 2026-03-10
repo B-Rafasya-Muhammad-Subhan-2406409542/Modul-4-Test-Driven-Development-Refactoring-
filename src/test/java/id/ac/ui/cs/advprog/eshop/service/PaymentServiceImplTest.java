7d99b9276581d7cf055320a45cf036accea1daf2
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-03-10 11:04:17 +0700

    [RED] Add tests for addPayment function

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImplTest.java
@@ -0,0 +1,63 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceImplTest {
+
+    @InjectMocks
+    private PaymentServiceImpl paymentService;
+
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    private Order order;
+    private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp() {
+        // Buat dummy product agar list tidak null / kosong
+        List<Product> products = new ArrayList<>();
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(2);
+        products.add(product);
+
+        // Masukkan list products ke dalam order
+        order = new Order("order-1", products, 1708560000L, "Safira");
+        paymentData = new HashMap<>();
+    }
+
+    @Test
+    void testAddPaymentBankTransferSuccess() {
+        paymentData.put("bankName", "BCA");
+        paymentData.put("referenceCode", "REF12345");
+        Payment payment = new Payment("payment-1", "BANK_TRANSFER", order, paymentData);
+
+        when(paymentRepository.save(any(Payment.class))).thenReturn(payment);
+
+        Payment result = paymentService.addPayment(order, "BANK_TRANSFER", paymentData);
+
+        assertEquals("SUCCESS", result.getStatus());
+        verify(paymentRepository, times(1)).save(any(Payment.class));
+    }
+}
